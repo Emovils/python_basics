473253dcec5c6c86a2fe7caeeedef48488538cf4
Emovil <oviliekenelucky@gmail.com>
2025-03-26 00:10:31 +0100

    014_write_into_json_dynamically.py

---
+++ 014_write_into_json_dynamically.py
@@ -0,0 +1,14 @@
+import json
+a = input("id")
+b = input("first_name")
+c = input("last_name")
+d = input("email")
+e = input("password")
+user_dict = {"id": a,
+             "first_name": b,
+             "last_name": c,
+             "email": d,
+             "password": e
+             }
+with open("015_register.json", "w") as my_file:
+          json.dump(user_dict, my_file)
