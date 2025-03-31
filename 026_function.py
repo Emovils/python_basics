0daefde87761e73845695c0b3206eb487e0a1b0b
Emovil <oviliekenelucky@gmail.com>
2025-03-31 23:59:05 +0100

    026_function.py

---
+++ 026_function.py
@@ -0,0 +1,16 @@
+def greet_user ():
+    print("hello, welcome to programming")
+greet_user()
+
+def add_number(a, b):
+    result = a + b
+    print(result)
+add_number(9, 3)
+    
+
+def mutiply_number(a, b):
+    a = (a * b)
+    print(a)
+mutiply_number(10, 6)
+    
+
