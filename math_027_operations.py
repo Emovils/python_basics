e656730643a68c811521494f6de89e28fa4098fe
Emovil <oviliekenelucky@gmail.com>
2025-04-03 21:28:55 +0100

    math_027_operations.py

---
+++ math_027_operations.py
@@ -0,0 +1,24 @@
+def sum():
+    a = 7
+    b = 8
+    c = a + b
+    print(c)
+
+def sub():
+    a = 10
+    b = 17
+    c = a - b
+    print(c)
+
+def mutiply():
+    a = 30
+    b = 2
+    c = a * b
+    print(c)
+
+
+def divide():
+    a = 12
+    b = 2
+    c = a / b
+    print(c)
