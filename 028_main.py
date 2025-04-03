4e95441ed56c4c548e4fa8f6e587a3e580ebdca0
Emovil <oviliekenelucky@gmail.com>
2025-04-03 21:42:19 +0100

    028_main.py

--- 028_main.py
+++ 028_main.py
@@ -1,21 +1,5 @@
-from math_operations import *
-a = 20
-b = 4
-c = (a + b)
-print(c)
-
-a = 100
-b = 87
-c = (a - b)
-print(c)
-
-a = 40
-b = 6
-c = (a / b)
-print(c)
-
-a = 50
-b = 5
-c = (a * b)
-print(c)
-
+from math_027_operations import *
+sum()
+sub()
+mutiply()
+divide()
