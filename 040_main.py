30d8fc7ede0b5256520641cc73e4dc0ac7262c50
Emovil <oviliekenelucky@gmail.com>
2025-04-11 09:59:18 +0100

    040_main.py

---
+++ 040_main.py
@@ -0,0 +1,11 @@
+from logic_039 import to_uppercase, get_today_date, count_vowels, generate_random_number, get_full_name   
+
+print("uppercase result:", to_uppercase("believe in the proccess"))
+
+print("today date:",get_today_date())
+
+print("number of vowels:", count_vowels("beautiful"))
+
+print("random number:", generate_random_number())
+
+print("full name:", get_full_name())
